e8ca9d898dcc14d280c36fcfb3c096ccf9603a40
Saxtonn <aleksligeza@gmail.com>
2018-02-10 18:57:21 +0100

    Added history filters

--- operations.js
+++ operations.js
@@ -10,16 +10,49 @@ var auth = require('./auth.js')
 //cycleId === '0' operacja zdefiniowana
 //cycleId !== '-1' && cycleId !== '-1' operacja podlegajaca pod zdefniowana
 
+router.get('/historyWithFilters/:skip/:amountFrom/:amountTo/:description/:dateSince/:dateTo/:type', auth.checkAuthenticated, async (req, res) => {
+    var user = req.userId;
+
+    var skip = parseInt(req.params.skip);
+    var amountFrom = parseInt(req.params.amountFrom);
+    var amountTo = parseInt(req.params.amountTo);
+    var description = req.params.description;
+    var dateSince = new Date(req.params.dateSince);
+    var dateTo = new Date(req.params.dateTo);
+    var type = parseInt(req.params.type);
+
+
+    var history = await Operation.find({ userId: user, cycleId: { $ne: '0' } })
+        .sort({ date: -1, createDate: -1 });
+
+    history = history.filter(function (element) {
+        if (element.date >= dateSince && element.date <= dateTo) {
+            if (element.amount >= amountFrom && element.amount <= amountTo) {
+                if (element.income === true && type == 1 ||
+                    element.income === false && type == 2 ||
+                    type === 0) {
+                    if (element.description.indexOf(description) !== -1 || description === '0null') {
+                        return element;
+                    }
+                }
+            }
+        }
+    });
+    history = history
+        .slice(skip, skip+10);
+
+    res.status(200).send(history);
+})
+
 router.get('/history/:skip', auth.checkAuthenticated, async (req, res) => {
     var user = req.userId;
     var skip = parseInt(req.params.skip);
     var history = await Operation.find({ userId: user, cycleId: { $ne: '0' } })
         .skip(skip)
         .limit(10)
-        .sort({ date: -1 , createDate: -1 });
+        .sort({ date: -1, createDate: -1 });
 
     res.status(200).send(history);
-
 })
 
 router.get('/details/:id', auth.checkAuthenticated, async (req, res) => {
@@ -51,8 +84,8 @@ router.post('/create', auth.checkAuthenticated, (req, res) => {
     operationData.userId = req.userId;
 
     var newOpeartion = new Operation(operationData);
-    newOpeartion.date.setHours(0,0,0,0);
     newOpeartion.createDate = Date();
+    newOpeartion.date.setUTCHours(12, 0, 0, 0);
 
     newOpeartion.save((err, result) => {
         if (err) {
@@ -69,7 +102,7 @@ router.put('/update', auth.checkAuthenticated, async (req, res) => {
     operationData.userId = req.userId;
 
     var newOpeartion = new Operation(operationData);
-    newOpeartion.date.setHours(0,0,0,0);
+    newOpeartion.date.setUTCHours(12, 0, 0, 0);
 
     var oldOperation = await Operation.findOne({ _id: operationData.id, userId: req.userId }, (err, result) => {
         if (err) {
